3e361b43bdb1262083a433747d6c58671803d443
sly <sylvaincadoret@hotmail.com>
2024-02-12 13:03:18 +0100

    backup

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+import "./style.scss";
+
+
+export default function Hero() {
+    const videoRef = useRef(null);
+  
+    useEffect(() => {
+      if (videoRef.current) {
+        videoRef.current.play();
+      }
+    }, []); // Le tableau vide signifie que cela ne s'exécute qu'une seule fois au montage du composant
+
+    return (
+
+        <div className="hero">
+        <div className="hero__video"></div>
+        <video ref={videoRef} id="video-background" loop playsInline autoPlay>
+          <source src="assets/hero2.mp4" type="video/mp4" />
+        </video>
+        <div className="hero__title">
+          <h1>
+            Smarter <span>. </span>Safer <span>. </span>
+          </h1>
+        </div>
+        </div>
+    );
+
+    };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,36 +1,27 @@
 import React, { useEffect, useRef } from "react";
 import "./style.scss";
 import CustomersSlider from "../../Components/CustomersSlider/CustomersSlider";
+import Hero from "../../Components/Hero/Hero";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
-  }, []); // Le tableau vide signifie que cela ne s'exécute qu'une seule fois au montage du composant
-
-  const customers = ["chanel.png", "ratp.png", "SG.png", "vinci.png"];
-
   return (
     <div className="main">
-      <div className="hero">
-        <div className="hero__video"></div>
-        <video ref={videoRef} id="video-background" loop playsInline autoPlay>
-          <source src="assets/hero.mp4" type="video/mp4" />
-        </video>
-        <div className="hero__title">
-          <h1>
-            Smarter<span>.</span>Safer<span>.</span>
-          </h1>
-        </div>
+      <Hero />
 
-        <div className="section-1">
-          <div className="section-1__intro">
-            <h2>Notre force</h2>
-            <p>
-              Une approche innovante pour la protection de vos données
+      <div className="section-1">
+        <div className="section-1__intro">
+          {/* <h2>Notre force</h2> */}
+          <p>
+            La <strong>sécurité</strong> ne dépend plus de la force brute, mais
+            de <strong>l'intelligence</strong>. Chez <strong>Pixecurity</strong>
+            , nous sommes les fournisseurs de systèmes de{" "}
+            <strong>sûreté</strong> intelligents. <br />
+            Notre <strong>mission</strong> : vous accompagner avec les{" "}
+            <strong>meilleurs</strong> produits et des{" "}
+            <strong>solutions</strong> sur-mesure, repensant la sécurité selon
+            vos besoins.
+            {/* Une approche innovante pour la protection de vos données
               personnelles : Avec Pixecurity, bénéficiez d'une panoplie d'outils
               électroniques, d'une plateforme de gestion de projets exclusive
               pour une interaction fluide avec nos experts, des développements
@@ -38,46 +29,52 @@ export default function Home() {
               consultants délivrent des certifications de conformité en matière
               de compliance, couvrant l'ensemble de la chaîne de valeur de la
               sûreté. Pixecurity, votre fournisseur de solutions de sûreté 3.0,
-              repense votre sécurité numérique
-            </p>
-          </div>
-
-          <div className="section-1__shop">
-            <h2>Découvrez nos produits</h2>
-            <p>
-              Nous proposons une sélection de produits spécialement pensés pour
-              répondre à vos besoins. Profitez des conseils et services
-              Pixecurity{" "}
-            </p>
-            <button>Voir le catalogue TEST </button>
-          </div>
+              repense votre sécurité numérique */}
+          </p>
         </div>
 
-        <div className="section-2">
-          <div className="section-2__services">
-            <h2>Nos services</h2>
-            <p>
-              Nous vous proposons une large gamme de services pour répondre à
-              vos besoins en matière de sécurité numérique. Nos experts vous
-              accompagnent dans la mise en place de solutions adaptées à votre
-              entreprise.
-            </p>
-            <button>Voir nos services</button>
-          </div>
-          <div className="section-2__contact">
-            <h2>Contactez-nous</h2>
-            <p>
-              Vous avez une question ? Vous souhaitez en savoir plus sur nos
-              services ? N'hésitez pas à nous contacter, nous serons ravis de
-              vous répondre.
-            </p>
-            <button>Nous contacter</button>
-          </div>
+        <div className="section-1__shop">
+          <h2>Découvrez nos produits</h2>
+          <p>
+            Nous proposons une sélection de produits spécialement pensés pour
+            répondre à vos besoins. Profitez des conseils et services Pixecurity{" "}
+          </p>
+          <button>
+            <NavLink to="/Produits">Voir le catalogue </NavLink>{" "}
+          </button>
         </div>
-        <div className="section-3">
-         
-          <CustomersSlider customers={customers} />
+      </div>
+
+      <div className="section-2">
+        <div className="section-2__force">
+          <h2>Notre force</h2>
+          <h3>Une offre pensée autrement</h3>
+          <p>
+            La protection de vos données personnelles certifiée Pixecurity c'est
+            : la fourniture d'outils électroniques, une plateforme de gestion de
+            projets réalisée en propre pour faciliter l'interaction avec nos
+            experts, des développements spécifiques et sur mesure, la mise en
+            place de solutions cyber. Des certifications de conformités
+            delivrées par nos consultants sur du compliance : Pixecurity couvre
+            toute la haute chaine de valeur de la sureté. Pixecurity est le
+            fournisseur de solutions de sureté 3.0
+          </p>
+          <button>
+            <NavLink to="#">Notre process</NavLink>{" "}
+          </button>
         </div>
+        <div className="section-2__contact">
+          <h2>Contactez-nous</h2>
+          <p>
+            Vous avez une question ? Vous souhaitez en savoir plus sur nos
+            services ? N'hésitez pas à nous contacter, nous serons ravis de vous
+            répondre.
+          </p>
+          <button>Nous contacter</button>
+        </div>
+      </div>
+      <div className="section-3">
+        <CustomersSlider />
       </div>
     </div>
   );
