be73d6069b948406ee772afc320aa7812091d2dc
sly <sylvaincadoret@hotmail.com>
2024-03-05 17:01:06 +0100

    ✅ Cart view starting

--- src/Components/useCart.jsx
+++ src/Components/useCart.jsx
@@ -1,67 +1,56 @@
-import React, { useState, useEffect } from 'react';
-
-// ce hook permet de gérer le panier
+import { useState } from 'react';
 
 const useCart = () => {
+  const [cart, setCart] = useState([]);
+  const [isAddingToCart, setIsAddingToCart] = useState(false);
 
-    const [cart, setCart] = useState([]);
-    const [isAddingToCart, setIsAddingToCart] = useState(false);
+  const addToCart = async (userId, productId, productName, productRef, quantity) => {
+    try {
+      setIsAddingToCart(true);
 
-    // const [quantity, setQuantity] = useState(1);
+      const response = await fetch(`http://localhost:3001/users/${userId}/add-cart/${productId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          product_id: productId,
+          name: productName,
+          ref: productRef,
+          quantity: quantity,
+        }),
+      });
 
-    const addToCart = async (userId, productId, productName, productRef, quantity) => {
-        try {
-            setIsAddingToCart(true);
+      if (response.ok) {
+        console.log('Produit ajouté au panier avec succès!');
+        return true;
+      } else {
+        console.error("Erreur lors de l'ajout du produit au panier");
+        return false;
+      }
+    } catch (error) {
+      console.error('Erreur réseau:', error);
+      return false;
+    } finally {
+      setIsAddingToCart(false);
+    }
+  };
 
-            const response = await fetch(`http://localhost:3001/users/${userId}/add-cart/${productId}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    product_id: productId,
-                    name: productName,
-                    ref : productRef,
-                    quantity: quantity
-                })
-            });
+  const fetchCart = async (userId) => {
+    try {
+      const response = await fetch(`http://localhost:3001/users/${userId}/cart`);
+      if (response.ok) {
+        const data = await response.json();
+        setCart(data);
+      } else {
+        console.error('Réponse du serveur:', response.status);
+      }
+    } catch (error) {
+      console.error('Erreur réseau:', error);
+    }
+  };
 
-            if (response.ok) {
-                console.log('Produit ajouté au panier avec succès!');
-                return true;
-            } else {
-                console.error('Erreur lors de l\'ajout du produit au panier');
-                return false;
-            }
-        } catch (error) {
-            console.error('Erreur réseau:', error);
-            return false;
-        } finally {
-            setIsAddingToCart(false);
-        }
-    };
-
-    const getCart = async (userId) => {
-        try {
-            const response = await fetch(`http://localhost:3001/users/${userId}/cart`);
-
-            if (response.ok) {
-                const data = await response.json();
-                setCart(data.cart);
-            } else {
-                console.error('Erreur lors de la récupération du panier:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Erreur réseau:', error);
-        }
-    };
-
-    useEffect(() => {
-        getCart(1);
-    }, []);
-
-    return { cart, isAddingToCart, addToCart };
-    
-}
+  return { cart, isAddingToCart, addToCart, fetchCart };
+};
 
 export default useCart;
--- src/Pages/Shop/Cart/Cart.jsx
+++ src/Pages/Shop/Cart/Cart.jsx
@@ -1,27 +1,68 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
-
-
-
+import useCart from '../../../Components/useCart';
 
 export default function Cart() {
-    
+  const { fetchCart, cart } = useCart();
+  const [data, setData] = useState([]);
+  const [prices, setPrices] = useState([]);
+
+  // Fetch product prices
+  useEffect(() => {
+    fetch('http://localhost:3001/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        // Assuming data is an array of product prices, update prices state accordingly
+        setPrices(data.map((product) => product.price));
+      })
+      .catch((error) => {
+        console.error('Error fetching product prices:', error);
+      });
+  }, []);
+
+  // Fetch cart when the component mounts
+  useEffect(() => {
+    const userDataString = localStorage.getItem('user');
+    const userData = JSON.parse(userDataString);
+    const userId = userData._id;
+    const total =
+        cart.length > 0
+            ? cart.reduce((acc, product, index) => {
+                return acc + product.quantity * prices[index];
+            }, 0)
+            : 0;
+    console.log('total', total);
 
 
+    fetchCart(userId);
+  }, []);
 
-
-
-return (
-    <div className="cart-container">
-        <h2>Panier</h2>
-        <div className="cart-items">
-         
+  return (
+    <div className='cart-container'>
+      <h1>Votre panier</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Produit</th>
+            <th>Référence</th>
+            <th>Quantité</th>
+            <th>Prix unitaire</th>
+            <th>Prix total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product, index) => (
+            <tr key={index}>
+              <td>{product.name}</td>
+              <td>{product.ref}</td>
+              <td>{product.quantity}</td>
+              <td>{prices[index]} €</td>
+                <td>{product.quantity * prices[index]} €</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-
-    </div>
-);
+  );
 }
-
-
-
-
