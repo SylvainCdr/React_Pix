1ba680c9d887984097c38660834fc95e9dbffb7d
sly <sylvaincadoret@hotmail.com>
2024-05-13 12:28:26 +0200

    ✅ shopNav upgrade

---
+++ src/Components/ShopAside/ShopAside.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.scss';
+
+export default function ShopAside() {
+
+    return (
+        <aside className="shopAside-container">
+         
+
+
+        </aside>
+    )
+
+}
