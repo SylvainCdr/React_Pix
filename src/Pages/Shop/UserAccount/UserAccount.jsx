2e8970a40262f8c73825ed860deab9a0ecbb4290
sly <sylvaincadoret@hotmail.com>
2024-05-21 15:40:28 +0200

    ✅ User Dashboard debug (userData is null)

--- src/Pages/Shop/UserAccount/UserAccount.jsx
+++ src/Pages/Shop/UserAccount/UserAccount.jsx
@@ -8,40 +8,66 @@ import AOS from "aos";
 import DeliveryTimeline from "../../../Components/DeliveryTimeline/DeliveryTimeline";
 
 export default function UserAccount() {
-  const userDataString = localStorage.getItem("user");
-  const userData = JSON.parse(userDataString);
-  const userId = userData._id;
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
   const [selectedTab, setSelectedTab] = useState("favoris");
   const [products, setProducts] = useState([]);
   const [favorites, setFavorites] = useState([]);
-  const navigate = useNavigate();
   const [orders, setOrders] = useState([]);
 
   const { getFavorites, removeFromFavorites, checkFavorite, addToFavorites } =
     useFavorites();
   const { addToCart } = useCart();
 
+  useEffect(() => {
+    const userDataString = localStorage.getItem("user");
+    if (userDataString) {
+      setUserData(JSON.parse(userDataString));
+    } else {
+      navigate("/login"); // Redirigez vers la page de connexion si l'utilisateur n'est pas connecté
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    if (userData) {
+      const fetchData = async () => {
+        try {
+          const [productsData, favoritesData] = await Promise.all([
+            fetch("http://localhost:3001/products").then((res) => res.json()),
+            getFavorites(userData._id),
+          ]);
+
+          setProducts(productsData);
+          setFavorites(favoritesData);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [userData, getFavorites]);
+
+  useEffect(() => {
+    if (userData) {
+      fetch(`http://localhost:3001/orders?userId=${userData._id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setOrders(data);
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la récupération des commandes :", error);
+        });
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   const handleTabClick = (tab) => {
     setSelectedTab(tab);
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const [productsData, favoritesData] = await Promise.all([
-          fetch("http://localhost:3001/products").then((res) => res.json()),
-          getFavorites(userId),
-        ]);
-
-        setProducts(productsData);
-        setFavorites(favoritesData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [userId]);
 
   const handleProductClick = (productId) => {
     const product = products.find((item) => item._id === productId);
@@ -51,23 +77,9 @@ export default function UserAccount() {
     }
   };
 
-  // Récupération des commandes comportant un champs avec l'id de l'utilisateur
-  useEffect(() => {
-   
-
-    fetch(`http://localhost:3001/orders?userId=${userId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setOrders(data);
-      })
-      .catch((error) => {
-        console.error("Erreur lors de la récupération des commandes :", error);
-      });
-  }, [userId]);
-
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
+  if (!userData) {
+    return null; // Ou affichez un message de chargement si nécessaire
+  }
 
   return (
     <div className="user-account-container">
@@ -106,7 +118,7 @@ export default function UserAccount() {
           <div data-aos="zoom-out-up" className="user-infos">
             <div className="grid-infos">
               <div className="perso">
-              <h4>Informations personnelles</h4>
+                <h4>Informations personnelles</h4>
                 <p>Nom : {userData.lastName}</p>
                 <p>Prénom : {userData.firstName}</p>
                 <p>Entreprise : {userData.company}</p>
@@ -127,14 +139,14 @@ export default function UserAccount() {
                 <p>Pays : {userData.billingAddress.country}</p>
               </div>
               <div className="contact">
-              <h4>Informations de contact</h4>
+                <h4>Informations de contact</h4>
                 <p>Email : {userData.email}</p>
                 <p>Téléphone : {userData.phone}</p>
               </div>
             </div>
-            
-              <NavLink to="/edit-account"><button>Modifier mes informations</button></NavLink>
-            
+            <NavLink to="/edit-account">
+              <button>Modifier mes informations</button>
+            </NavLink>
           </div>
         )}
 
@@ -148,17 +160,15 @@ export default function UserAccount() {
                   );
 
                   return (
-                    
-                    <div data-aos="fade-up-left">
-                    <ProductCard
-                      key={favorite.product_id}
-                      product={product}
-                      addToCart={addToCart}
-                      addToFavorites={addToFavorites}
-                      checkFavorite={checkFavorite}
-                      removeFromFavorites={removeFromFavorites}
-                      handleProductClick={handleProductClick}
-                    />
+                    <div data-aos="fade-up-left" key={favorite.product_id}>
+                      <ProductCard
+                        product={product}
+                        addToCart={addToCart}
+                        addToFavorites={addToFavorites}
+                        checkFavorite={checkFavorite}
+                        removeFromFavorites={removeFromFavorites}
+                        handleProductClick={handleProductClick}
+                      />
                     </div>
                   );
                 })
@@ -172,60 +182,55 @@ export default function UserAccount() {
         {selectedTab === "commandes" && (
           <div data-aos="fade-up" className="user-orders">
             {orders.length > 0 ? (
-              orders.map((order) => {
-                return (
-                  <div className="order" key={order._id}>
-                    <table>
-                      <thead>
-                        <tr>
-                          <th>N° de commande</th>
-                          <th>Date</th>
-                          <th>Produits</th>
-                          <th>Total</th>
-                          {/* <th>Statut</th> */}
-                          <th>Détails</th>
-                        </tr>
-                        {/* // ajout d'une ligne pour afficher la timeline */}
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>{order._id}</td>
+              orders.map((order) => (
+                <div className="order" key={order._id}>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>N° de commande</th>
+                        <th>Date</th>
+                        <th>Produits</th>
+                        <th>Total</th>
+                        {/* <th>Statut</th> */}
+                        <th>Détails</th>
+                      </tr>
+                      {/* // ajout d'une ligne pour afficher la timeline */}
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{order._id}</td>
 
-                          <td>
-                            {order.orderDate
-                              ? new Date(order.orderDate).toLocaleDateString()
-                              : "Date inconnue"}
-                          </td>
+                        <td>
+                          {order.orderDate
+                            ? new Date(order.orderDate).toLocaleDateString()
+                            : "Date inconnue"}
+                        </td>
 
-                          <td>
-                            {order.items.map((product) => (
-                              <p key={product._id}>
-                                {product.name} <span>x {product.quantity}</span>
-                              </p>
-                            ))}
-                          </td>
-                          {/* // Calcul du total de la commande, on arrondi à 2 chiffres après la virgule */}
-                          <td> {order.totalAmount.toFixed(2)} € </td>
-                          {/* <td>{order.status}</td> */}
-                          <td>
+                        <td>
+                          {order.items.map((product) => (
+                            <p key={product._id}>
+                              {product.name} <span>x {product.quantity}</span>
+                            </p>
+                          ))}
+                        </td>
+                        {/* // Calcul du total de la commande, on arrondi à 2 chiffres après la virgule */}
+                        <td> {order.totalAmount.toFixed(2)} € </td>
+                        {/* <td>{order.status}</td> */}
+                        <td>
                           <NavLink to={`/mon-compte/commande/${order._id}`}>
-                            <button>
-                              
-                                Voir
-                            </button>
-                              </NavLink>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td colSpan="5">
-                            <DeliveryTimeline status={order.status} />
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                );
-              })
+                            <button>Voir</button>
+                          </NavLink>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colSpan="5">
+                          <DeliveryTimeline status={order.status} />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              ))
             ) : (
               <p>Vous n'avez pas encore de commandes.</p>
             )}
