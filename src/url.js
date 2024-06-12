e1e28fd813c1dbe99529aef1af45678d15eab3cc
sly <sylvaincadoret@hotmail.com>
2024-06-12 09:09:27 +0200

    url update

--- src/url.js
+++ src/url.js
@@ -1,4 +1,3 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "http://localhost:3001" || process.env.REACT_APP_BASE_URL;
 console.log(BASE_URL)
 
-// process.env.REACT_APP_BASE_URL || 
