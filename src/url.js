fba6e08abd9a099a0c632256af34b2fca2563217
sly <sylvaincadoret@hotmail.com>
2024-06-18 11:16:58 +0200

    url change

--- src/url.js
+++ src/url.js
@@ -1,5 +1,3 @@
 export const BASE_URL =
-  process.env.REACT_APP_BASE_URL  ||  "http://localhost:3001" 
-
-//   || "https://agile-brushlands-56076-16e3e6016b28.herokuapp.com"; 
-// console.log(BASE_URL);
+  process.env.REACT_APP_BASE_URL  ||  "http://localhost:3001" || "https://agile-brushlands-56076-16e3e6016b28.herokuapp.com"; 
+console.log(BASE_URL);
